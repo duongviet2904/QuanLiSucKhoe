0289f791ab1c534d0bb40b4fd0e514aac0b2441a
duongviet2904 <89138046+duongviet2904@users.noreply.github.com>
2022-06-02 11:49:06 +0700

    duongv

---
+++ src/object/GiaoVien.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package object;
+
+/**
+ *
+ * @author duongv
+ */
+public class GiaoVien extends TaiKhoan{
+    private String maGV;
+    private String tenGV;
+    
+    private String maKhoa;
+
+    public GiaoVien(String maGV, String tenGV, String maKhoa, String maTK, String tenDangNhap, String matKhau, int vaiTro) {
+        super(maTK, tenDangNhap, matKhau, vaiTro);
+        this.maGV = maGV;
+        this.tenGV = tenGV;
+        this.maKhoa = maKhoa;
+    }
+
+    public String getMaGV() {
+        return maGV;
+    }
+
+    public void setMaGV(String maGV) {
+        this.maGV = maGV;
+    }
+
+    public String getTenGV() {
+        return tenGV;
+    }
+
+    public void setTenGV(String tenGV) {
+        this.tenGV = tenGV;
+    }
+
+    public String getMaKhoa() {
+        return maKhoa;
+    }
+
+    public void setMaKhoa(String maKhoa) {
+        this.maKhoa = maKhoa;
+    }
+    
+}
